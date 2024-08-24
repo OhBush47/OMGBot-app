397432a98ffab8faf3cfddd3aa597b009dae680d
OhBush47 <olivier.buissonca@gmail.com>
2024-08-24 14:53:10 -0400

    pnl

--- OMGBot.py
+++ OMGBot.py
@@ -14,8 +14,12 @@ db = st.secrets['db']
 sql_engine = create_engine(f"mysql+mysqlconnector://{db_user}:{db_pw}@{db_host}:{db_port}/{db}", pool_recycle=3600, pool_pre_ping=True)
 sql_engine = sql_engine.execution_options(autocommit=True)
 
+#Date and investment
+ethinvestment=pd.read_sql("""SELECT sum(InvestmentETH) as InvestmentETH FROM thememes6529.investors""",sql_engine)
+
+#Data
 df=pd.read_sql("""SELECT BIDASKS.TimeStamp
-, ETHWETH.ETHWETHBal + sum(BIDASKS.TokenBal * BIDASKS.Bid)  NAVBid
+, ETHWETH.ETHWETHBal + sum(BIDASKS.TokenBal * BIDASKS.Bid) as NAVBid
 , ETHWETH.ETHWETHBal + sum(BIDASKS.TokenBal * BIDASKS.Ask) as NAVAsk 
 FROM thememes6529.bidasks BIDASKS
 LEFT JOIN thememes6529.ethweth ETHWETH
@@ -24,6 +28,18 @@ WHERE BIDASKS.TimeStamp >= '2023-04-06 14:03:54'
 and BIDASKS.TimeStamp in (select max(TimeStamp) from thememes6529.bidasks group by Date(TimeStamp))
 group by BIDASKS.TimeStamp, ETHWETH.ETHWETHBal""", sql_engine)
 
+#Calc Returns
+max_ts = df.TimeStamp.max()
+navbid = df[df.TimeStamp == max_ts]
+navask = df[df.TimeStamp == max_ts]
+returnsbid = navbid / ethinvestment - 1
+returnsask = navask / ethinvestment - 1
+
+bidcol, askcol = st.columns(2)
+bidcol.write(returnsbid)
+askcol.write(returnsask)
+
+#Chart
 st.line_chart(df, x="TimeStamp",y=["NAVBid","NAVAsk"], height=666)
 
 sql_engine.dispose()
