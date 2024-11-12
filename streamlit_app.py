1c2184f2f6259e9dd071783114a43350b1e7de6f
OhBush47 <olivier.buissonca@gmail.com>
2024-11-12 05:28:25 -0500

    logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.logo("omg.jpg")
+
 pg_valuationmetrics = st.Page("valuationmetrics.py", title="Valuation Metrics")
 pg_bidaskts = st.Page("bidasktimeseries.py", title="Bid Ask TimeSeries")
 pg_omgbot = st.Page("OMGBot.py", title="OMGBot")
