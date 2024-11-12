6a15c958e548c6df8ebf25b10624249c9b0c80a5
OhBush47 <olivier.buissonca@gmail.com>
2024-11-12 05:39:18 -0500

    NAKA

--- bidasktimeseries.py
+++ bidasktimeseries.py
@@ -16,11 +16,19 @@ db = st.secrets['db']
 sql_engine = create_engine(f"mysql+mysqlconnector://{db_user}:{db_pw}@{db_host}:{db_port}/{db}", pool_recycle=3600, pool_pre_ping=True)
 sql_engine = sql_engine.execution_options(autocommit=True)
 
-df=pd.read_sql("""SELECT 
+df=pd.read_sql("""               
+SELECT 
 BIDASKS.TimeStamp
-, sum(BIDASKS.Bid) as Bid
-, sum(BIDASKS.Ask) as Ask 
-FROM thememes6529.bidasks BIDASKS
+, sum(CASE WHEN BIDASKS.Bid < NAKA.Bid THEN BIDASKS.Bid ELSE 0 END) as Bid
+, sum(CASE WHEN BIDASKS.Ask < NAKA.Ask THEN BIDASKS.Ask ELSE 0 END) as Ask
+FROM 
+    thememes6529.bidasks BIDASKS
+
+JOIN 
+    thememes6529.bidasks NAKA
+    on BIDASKS.TimeStamp = NAKA.TimeStamp
+    and NAKA.TokenID = 4
+               
 WHERE BIDASKS.TimeStamp in (SELECT MAX(TimeStamp) FROM thememes6529.bidasks GROUP BY DATE(TimeStamp))
 GROUP BY BIDASKS.TimeStamp
 ORDER by BIDASKS.TimeStamp""", sql_engine)
