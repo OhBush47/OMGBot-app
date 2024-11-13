69e7b0707066d8a419eb1de6db27ee17634f8a51
OhBush47 <olivier.buissonca@gmail.com>
2024-11-13 05:12:44 -0500

    timeseries

--- bidasktimeseries.py
+++ bidasktimeseries.py
@@ -16,11 +16,25 @@ db = st.secrets['db']
 sql_engine = create_engine(f"mysql+mysqlconnector://{db_user}:{db_pw}@{db_host}:{db_port}/{db}", pool_recycle=3600, pool_pre_ping=True)
 sql_engine = sql_engine.execution_options(autocommit=True)
 
+#Get szns & tokens
+szns = pd.read_sql("SELECT DISTINCT Szn FROM thememes6529.stats").tolist()
+tokens = pd.read_sql("SELECT DISTINCT TokenID FROM thememes6529.stats").tolist()
+
+col_szns, col_tokens = st.columns(2)
+box_szns = col_szns.multiselect("Szns:",szns)
+box_tokens = col_tokens.multiselect("Tokens":tokens)
+select_szns = ", ".join(box_szns)
+select_tokens =", ".join(box_tokens)
+st.write(select_szns)
+st.write(select_tokens)
+
 df=pd.read_sql("""               
 SELECT 
-BIDASKS.TimeStamp
-, sum(CASE WHEN BIDASKS.Bid < NAKA.Bid THEN BIDASKS.Bid ELSE 0 END) as Bid
-, sum(CASE WHEN BIDASKS.Ask < NAKA.Ask THEN BIDASKS.Ask ELSE 0 END) as Ask
+    BIDASKS.TimeStamp
+    , sum(CASE WHEN BIDASKS.Avg < NAKA.Avg THEN BIDASKS.Avg ELSE 0 END) as Avg
+    , sum(CASE WHEN BIDASKS.Bid < NAKA.Bid THEN BIDASKS.Bid ELSE 0 END) as Bid
+    , sum(CASE WHEN BIDASKS.Ask < NAKA.Ask THEN BIDASKS.Ask ELSE 0 END) as Ask
+
 FROM 
     thememes6529.bidasks BIDASKS
 
@@ -29,10 +43,19 @@ JOIN
     on BIDASKS.TimeStamp = NAKA.TimeStamp
     and NAKA.TokenID = 4
                
-WHERE BIDASKS.TimeStamp in (SELECT MAX(TimeStamp) FROM thememes6529.bidasks GROUP BY DATE(TimeStamp))
-GROUP BY BIDASKS.TimeStamp
-ORDER by BIDASKS.TimeStamp""", sql_engine)
+JOIN 
+    thememes6529.stats STATS
+    on BIDASKS.TokenID = STATS.TokenID
+               
+WHERE 
+    STATS.Szn in ('{select_szns}')
+    AND STATS.TokenID in ('{select_tokens}')
 
-st.line_chart(df, x="TimeStamp",y=["Bid","Ask"], height=666, use_container_width=True)
+    BIDASKS.TimeStamp in (SELECT MAX(TimeStamp) FROM thememes6529.bidasks GROUP BY DATE(TimeStamp))
+    GROUP BY BIDASKS.TimeStamp
+    ORDER by BIDASKS.TimeStamp
+""", sql_engine)
+
+st.line_chart(df, x="TimeStamp",y=["Bid","Ask","Avg"], height=666, use_container_width=True)
 
 sql_engine.dispose()
