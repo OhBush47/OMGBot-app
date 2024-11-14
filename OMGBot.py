3d19b764fe9d96d8385a0cb8a005f8f128407cad
OhBush47 <olivier.buissonca@gmail.com>
2024-11-14 04:48:41 -0500

    getchart

--- OMGBot.py
+++ OMGBot.py
@@ -18,40 +18,38 @@ sql_engine = sql_engine.execution_options(autocommit=True)
 #Date and investment
 investment=pd.read_sql("""SELECT sum(InvestmentETH) as InvestmentETH FROM thememes6529.investors""",sql_engine).InvestmentETH.iloc[0]
 
-#Data
-df=pd.read_sql("""SELECT BIDASKS.TimeStamp
-, ETHWETH.ETHBal + ETHWETH.WETHBal as ETHWETH
-, ETHWETH.OTHERBal as COINS
-, sum(BIDASKS.TokenBal * BIDASKS.Ask) as NFTS
-FROM thememes6529.bidasks BIDASKS
-LEFT JOIN thememes6529.ethweth ETHWETH
-ON BIDASKS.TimeStamp = ETHWETH.TimeStamp
-WHERE BIDASKS.TimeStamp >= '2023-04-06 14:03:54'
-and BIDASKS.TimeStamp in (select max(TimeStamp) from thememes6529.bidasks group by Date(TimeStamp))
-group by BIDASKS.TimeStamp, ETHWETH.ETHBal, ETHWETH.WETHBal, ETHWETH.OTHERBal""", sql_engine)
-df.fillna(0,inplace=True)
+def Chart(bidask):
+    #Data
+    df=pd.read_sql(f"""SELECT BIDASKS.TimeStamp
+    , ETHWETH.ETHBal + ETHWETH.WETHBal as ETHWETH
+    , ETHWETH.OTHERBal as COINS
+    , sum(BIDASKS.TokenBal * BIDASKS.{bidask}) as NFTS
+    FROM thememes6529.bidasks BIDASKS
+    LEFT JOIN thememes6529.ethweth ETHWETH
+    ON BIDASKS.TimeStamp = ETHWETH.TimeStamp
+    WHERE BIDASKS.TimeStamp >= '2023-04-06 14:03:54'
+    and BIDASKS.TimeStamp in (select max(TimeStamp) from thememes6529.bidasks group by Date(TimeStamp))
+    group by BIDASKS.TimeStamp, ETHWETH.ETHBal, ETHWETH.WETHBal, ETHWETH.OTHERBal""", sql_engine)
 
-#Returns
-max_ts = df.TimeStamp.max()
-nav = df[df.TimeStamp == max_ts][['ETHWETH','COINS','NFTS']].sum(axis=1).iloc[0]
-st.write(nav)
-returns = nav / investment - 1
-returns *= 100
+    #Returns
+    max_ts = df.TimeStamp.max()
+    nav = df[df.TimeStamp == max_ts][['ETHWETH','COINS','NFTS']].sum(axis=1).iloc[0]
+    returns = nav / investment - 1
+    returns *= 100
+    st.subheader(f"Returns: {round(returns,2)}%")
 
-#Melt
-df_melted = df.melt('TimeStamp', var_name='Asset', value_name='ETH')
+    #Chart
+    df_melted = df.melt('TimeStamp', var_name='Asset', value_name='ETH')
+    chart = alt.Chart(df_melted).mark_bar().encode(
+        x=alt.X('TimeStamp', title='TimeStamp'),
+        y=alt.Y('sum(ETH)', title='ETH'),
+        color='Asset'
+    ).properties(
+        width=200
+    )
+    st.altair_chart(chart, use_container_width=True)
 
-#Metrics
-st.subheader(f"Returns: {round(returns,2)}%")
-
-#Chart
-chart = alt.Chart(df_melted).mark_bar().encode(
-    x=alt.X('TimeStamp', title='TimeStamp'),
-    y=alt.Y('sum(ETH)', title='ETH'),
-    color='Asset'
-).properties(
-    width=200
-)
-st.altair_chart(chart, use_container_width=True)
+Chart('Bid')
+Chart('Ask')
 
 sql_engine.dispose()
